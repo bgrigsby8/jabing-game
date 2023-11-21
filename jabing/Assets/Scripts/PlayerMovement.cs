066172adb79519c1626d6cc6ab556608c72131ac
Brad Grigsby <bgrigsby8@gmail.com>
2023-11-21 15:22:22 -0500

    Added a few game objects and added player movement and jumping to the PlayerMovement script

--- jabing/Assets/Scripts/PlayerMovement.cs
+++ jabing/Assets/Scripts/PlayerMovement.cs
@@ -1,28 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private bool isGrounded;
+    public float jumpForce = 8f;
+    public float moveSpeed = 10f;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 3;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveSpeed = 10;
-        //Define the speed at which the object moves.
+        Move();
+        if ((Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W)) && isGrounded)
+        {
+            Jump();
+        }
+    }
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        //Get the value of the Horizontal input axis.
+    void Move()
+    {
+        // Handles moving the player along the x-axis
+        float xDirection = Input.GetAxis("Horizontal");
+        Vector2 moveVector = new Vector2(xDirection * moveSpeed, rb.velocity.y);
+        rb.velocity = moveVector;
 
-        float verticalInput = Input.GetAxis("Vertical");
-        //Get the value of the Vertical input axis.
+        if (xDirection > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (xDirection < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
 
-        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime);
-        //Move the object to XYZ coordinates defined as horizontalInput, 0, and verticalInput respectively.
+    void Jump()
+    {
+        // Handles jumping the player
+        Vector2 jumpVector = new Vector2(0F, jumpForce);
+        rb.AddForce(jumpVector, ForceMode2D.Impulse);
+        isGrounded = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
